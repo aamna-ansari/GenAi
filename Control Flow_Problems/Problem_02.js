c87536a149bdd66bda11078c16797302d87100ab
AaMna29 <aamnansari29@gmail.com>
2024-02-29 23:51:11 +0500

    problems

---
+++ Control Flow_Problems/Problem_02.js
@@ -0,0 +1,11 @@
+// Javascript Program to Check if a number is Positive, Negative, or Zero
+
+let num = prompt("Enter a number");
+
+if (num > 0) {
+  console.log(`Hey, ${num} is a positive number`);
+} else if (num == 0) {
+  console.log(`Hey, ${num} is a zero`);
+} else {
+  console.log(`Hey, ${num} is negative number`);
+}

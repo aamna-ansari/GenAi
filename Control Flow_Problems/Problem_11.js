37768a7c1d4dcca2a0371536d09b94db36f6fab7
AaMna29 <aamnansari29@gmail.com>
2024-03-12 17:04:20 +0500

    'question'

--- Control Flow_Problems/Problem_11.js
+++ Control Flow_Problems/Problem_11.js
@@ -1,1 +1,20 @@
-// 
+//  Program to Make a Simple Calculator
+
+let num1 = parseFloat(prompt("Enter Your number"));
+let num2 = parseFloat(prompt("Enter your number"));
+
+let result;
+let operator = prompt("Enter Your Operator");
+if (operator == "+") {
+  result = num1 + num2;
+} else if (operator == "-") {
+  result = num1 - num2;
+} else if (operator == "/") {
+  result = num1 / num2;
+} else if (operator == "%") {
+  result = num1 % num2;
+} else {
+  result = num1 * num2;
+}
+
+console.log(`${num1} ${operator} ${num2} is equal to ${result}`);

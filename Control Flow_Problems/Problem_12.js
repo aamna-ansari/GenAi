b93d2d92e156cdef27edf8664e08d3258173bf58
AaMna29 <aamnansari29@gmail.com>
2024-03-15 19:46:39 +0500

    'change'

--- Control Flow_Problems/Problem_12.js
+++
@@ -1,9 +0,0 @@
-// Program to Find the Sum of Natural Numbers
-
-let num = parseInt(prompt("Enter the Number"));
-let sum = 0;
-
-for (let i = 1; i < num; i++) {
-  sum += i;
-}
-console.log(`The Sum of ${num} is ${sum}`);
--- Control Flow_Problems/Problem_16.js
+++
@@ -1,8 +0,0 @@
-// Program to Find the Factors of a Number
-let num = prompt("Enter the number:");
-console.log(`The factors of ${num} is:`);
-for (let i = 1; i < num; i++) {
-  if (num % i == 0) {
-    console.log(i);
-  }
-}

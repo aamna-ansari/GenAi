4601769682d097421e7d5db37b561b3863ac1c5f
aamna-ansari <aamnansari29@gmail.com>
2024-09-07 22:07:38 +0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+def main():
+    st.title("Simple Calculator")
+
+    # Input numbers
+    num1 = st.number_input("Enter the first number", format="%f")
+    num2 = st.number_input("Enter the second number", format="%f")
+
+    # Dropdown menu for operation selection
+    operation = st.selectbox("Choose the operation", ("Addition", "Subtraction", "Multiplication", "Division"))
+
+    # Perform the calculation
+    if operation == "Addition":
+        result = num1 + num2
+        st.success(f"The result is: {result}")
+
+    elif operation == "Subtraction":
+        result = num1 - num2
+        st.success(f"The result is: {result}")
+
+    elif operation == "Multiplication":
+        result = num1 * num2
+        st.success(f"The result is: {result}")
+
+    elif operation == "Division":
+        if num2 != 0:
+            result = num1 / num2
+            st.success(f"The result is: {result}")
+        else:
+            st.error("Division by zero is not allowed")
+
+if __name__ == "__main__":
+    main()
